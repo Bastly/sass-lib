6374d772c763ca9cc25e7c3d95aeddd7d55755c9
Alex Guerrero <alexguerrero1092@gmail.com>
2015-04-07 15:30:47 +0200

    Use gulp to automatize linting and testing

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+
+var paths = {
+    js: ['./gulpfile.js', './test/**/*.js'],
+    scss: ['./stylesheets/_sass-lib.scss'],
+    test: ['./test/**/*.js']
+};
+
+var plumberOpts = {};
+
+gulp.task('js-lint', function () {
+    return gulp.src(paths.js)
+        .pipe($.jshint('.jshintrc'))
+        .pipe($.plumber(plumberOpts))
+        .pipe($.jscs())
+        .pipe($.jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('scss-lint', function() {
+    gulp.src(paths.scss)
+        .pipe($.scssLint())
+        .pipe($.plumber(plumberOpts));
+});
+
+gulp.task('test', function () {
+    return gulp.src(paths.test)
+        .pipe($.plumber(plumberOpts))
+        .pipe($.mocha());
+});
+
+gulp.task('lint', ['scss-lint', 'js-lint']);
+gulp.task('test', ['lint']);
+
+gulp.task('watch', ['test'], function () {
+    gulp.watch(paths.js, ['js-lint']);
+    gulp.watch(paths.scss, ['scss-lint']);
+    gulp.watch(paths.test, ['test']);
+});
+
+gulp.task('watch-scss', function () {
+    gulp.watch(paths.scss, ['scss-lint']);
+});
+
+gulp.task('default', ['scss-lint', 'js-lint', 'test']);
