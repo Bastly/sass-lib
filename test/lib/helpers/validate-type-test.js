49918a320a2aadae0be1765724d5804a4073e418
Alex Guerrero <alexguerrero1092@gmail.com>
2015-04-07 17:40:43 +0200

    Use a dummy test to see if mocha works

--- test/lib/helpers/validate-type-test.js
+++ test/lib/helpers/validate-type-test.js
@@ -1,36 +1,29 @@
-var sass = require('node-sass');
-var includePaths = '../stylesheets/lib-sass';
+// var sass = require('node-sass');
+var expect = require('chai').expect;
+// var includePaths = '../stylesheets/lib-sass';
 
 describe('validate type function', function() {
-    var error = false;
-
-    it('should return an error message when (1px "string" "") is passed', function(done) {
-        sass.render({
-           data: '$error: validate-type(1px "string" "")',
-           includePaths: includePaths
-        },
-        function(error, result) { // node-style callback from v3.0.0 onwards
-            if (error) {
-                error = true;
-                done();
-            }
-        });
-
-        expect(error).toBe(true);
-    });
     it('should return an error message when (1px "string" "") is passed', function() {
-        sass.render({
-           data: '$error: validate-type(1px "string" "")',
-           includePaths: includePaths
-        },
-        function(error, result) { // node-style callback from v3.0.0 onwards
-            if (error) {
-                console.log(error.status); // used to be "code" in v2x and below
-                console.log(error.column);
-                console.log(error.message);
-                console.log(error.line);
-            } else {
-            }
-        });
+        var foo = true;
+        expect(foo).to.equal(true);
     });
 });
+
+// describe('validate type function', function() {
+//     var error = false;
+
+//     it('should return an error message when (1px "string" "") is passed', function(done) {
+//         sass.render({
+//         data: '$error: validate-type(1px "string" "")',
+//         includePaths: includePaths
+//     },
+//         function(error) { // node-style callback from v3.0.0 onwards
+//             if (error) {
+//                 error = true;
+//                 done();
+//             }
+//         });
+
+//         expect(error).not.toBe(true);
+//     });
+// });
