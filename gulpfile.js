930e8ec3d166e570ca57ae03b8eaa6f4e841322a
Alex Guerrero <alexguerrero1092@gmail.com>
2015-04-07 21:43:27 +0200

    Minor style fixes

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,12 @@ var $ = require('gulp-load-plugins')();
 var paths = {
     js: ['./gulpfile.js', './test/**/*.js'],
     scss: ['./stylesheets/_sass-lib.scss'],
-    test: ['./test/**/*.js']
+    test: './test/**/*.js'
 };
 
 var plumberOpts = {};
 
-gulp.task('js-lint', function () {
+gulp.task('js-lint', function() {
     return gulp.src(paths.js)
         .pipe($.jshint('.jshintrc'))
         .pipe($.plumber(plumberOpts))
@@ -25,19 +25,18 @@ gulp.task('scss-lint', function() {
         .pipe($.plumber(plumberOpts));
 });
 
-gulp.task('test', function () {
+gulp.task('test', function() {
     return gulp.src(paths.test, {read: false})
         .pipe($.plumber(plumberOpts))
         .pipe($.mocha({reporter: 'nyan'}));
 });
 
 gulp.task('lint', ['scss-lint', 'js-lint']);
-gulp.task('test', ['lint']);
 
-gulp.task('watch', ['test'], function () {
-    gulp.watch(paths.js, ['js-lint']);
-    gulp.watch(paths.scss, ['scss-lint']);
-    gulp.watch(paths.test, ['test']);
+gulp.task('watch', 'test', function() {
+    gulp.watch(paths.js, 'js-lint');
+    gulp.watch(paths.scss, 'scss-lint');
+    gulp.watch(paths.test, 'test');
 });
 
 gulp.task('default', ['scss-lint', 'js-lint', 'test']);
